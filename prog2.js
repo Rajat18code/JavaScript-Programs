7c95b54c2638d85971a5f968e74c4b747fa082a4
Rajat Chandra <rajatchandra1806@gmail.com>
2026-08-05 23:19:21 +0530

    Add files via upload

---
+++ prog2.js
@@ -0,0 +1,6 @@
+const prompt = require("prompt-sync")();
+
+let num1 = parseInt(prompt("Enter first number: "));
+let num2 = parseInt(prompt("Enter second number: "));
+
+console.log("Sum =", num1 + num2);
